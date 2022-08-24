2d32016de2e2f6678a4e1e6c7792ea36a231a58e
Jose Cleiton <cleitons835@gmail.com>
2022-08-24 12:34:35 -0300

    Init

---
+++ app/backend/src/database/interface/iUser.ts
@@ -0,0 +1,10 @@
+
+
+export default interface iUser {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+
+}
---
+++ app/backend/src/database/models/user.model.ts
@@ -0,0 +1,50 @@
+
+import { DataTypes, Model } from 'sequelize/types';
+import db from '.';
+import iUser from '../interface/iUser';
+
+
+
+class User extends Model implements iUser {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+User.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER.UNSIGNED,
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    tableName: 'users',
+    modelName: 'users',
+    sequelize: db,
+    underscored: true,
+    timestamps: false,
+  },
+);
+
+export default User;
