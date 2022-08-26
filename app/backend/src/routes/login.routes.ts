71507c35db1a8355c795dc18a580a22d3c6ed768
Jose Cleiton <cleitons835@gmail.com>
2022-08-26 18:41:59 -0300

    voltando

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import Validation from '../Middleware/validation/Validation';
 import LoginController from '../controllers/LoginController';
 
 const loginRoutes = Router();
 
 loginRoutes.post('/', new LoginController().login);
-loginRoutes.get('/validate', Validation.validateLogin, LoginController.loginValidation);
+loginRoutes.get('/validate', LoginController.loginValidation);
 export default loginRoutes;
